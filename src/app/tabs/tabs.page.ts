22ba189b356b59ccbf3cb417c493d24c34e96fb3
Jeropa2 <huincamanjeronimo.dev@gmail.com>
2025-03-30 22:41:58 -0300

    `Feat: Tabs.page - titleService & Menu Button Global` | Se implementa title service y boton de menu global. - Se agrega y configura para que el menu lateral este disponible en toda la aplicacion sin necesidad de implementarlo en cada pagina por separado. - Se maneja la subscripcion al titleService para setear el titulo del modulo que este abierto en cada momento.

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuController, NavController } from '@ionic/angular';
+import { TitleService } from '../services/titleService/title.service';
 
 @Component({
   selector: 'app-tabs',
@@ -6,8 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss'],
   standalone: false,
 })
-export class TabsPage {
+export class TabsPage implements OnInit {
+  title: string = ''; // Titulo inicial vacío
 
-  constructor() {}
 
+  constructor(
+    private menuCtrl: MenuController,
+    private navCtrl: NavController,
+    private _titleService: TitleService
+  ) { }
+
+  ngOnInit() {
+    this._titleService.currentTitle$.subscribe((title: string) => {
+      this.title = title; // Actualiza el título desde el servicio
+    })
+  }
+
+  // Cierra el menú cuando un ítem es seleccionado
+  closeMenu() {
+    this.menuCtrl.close();
+  }
+
+  logout() {
+    this.navCtrl.navigateRoot('/login');
+    this.menuCtrl.close();
+  }
 }
